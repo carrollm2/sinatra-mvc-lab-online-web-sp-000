e5e1ecc87fa0a6bd87ce810b749d74b03f9fda17
Michael Carroll <carrollm2@gmail.com>
2020-03-01 21:29:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,8 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/piglatinize' do
+  # post '/piglatinize' do
+  post '/params[:user_phrase]' do
 
     @user_phrase = params[:user_phrase]
 
