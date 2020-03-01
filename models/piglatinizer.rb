d564a4c6a8bb5685b13186e48ee96a4ae1d92ac1
Michael Carroll <carrollm2@gmail.com>
2020-03-01 20:51:58 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,20 +1,5 @@
 class PigLatinizer
 
-  # def piglatinize(word)
-  #
-  #   vowel_list = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-  #   split_index_start = word.index(/[aeiouAEIOU]/)
-  #
-  #   if vowel_list.include?(word.split('').first)
-  #     pig_latin_end_of_word = 'way'
-  #   else
-  #     pig_latin_end_of_word = 'ay'
-  #   end
-  #
-  #   pig_latin_word = word.slice(split_index_start..-1) + word.slice(0, split_index_start) + pig_latin_end_of_word
-  #
-  # end
-
   def piglatinize(word)
 
     vowel_list = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
@@ -41,7 +26,7 @@ class PigLatinizer
     end
 
     pig_latin_word
-    
+
   end
 
 end
